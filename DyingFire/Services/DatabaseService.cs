0f2ad50418d168d4163e90b421c6cdd672e71d79
S00250496 <S00250496@atu.ie>
2026-02-17 16:19:33 +0000

    fixing DB

--- DyingFire/Services/DatabaseService.cs
+++ DyingFire/Services/DatabaseService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
-using System.Linq; // Needed for LINQ queries
+using System.Linq;
 using DyingFire.Models;
 
 namespace DyingFire.Services
@@ -33,53 +33,53 @@ namespace DyingFire.Services
                     while (reader.Read())
                     {
                         var loc = new Location(
-                            (int)reader["ID"],
+                            Convert.ToInt32(reader["ID"]),
                             reader["ImagePath"].ToString(),
                             reader["Name"].ToString()
                         );
 
-                        // Set Directions
-                        loc.LocationToNorth = (int)reader["NorthID"];
-                        loc.LocationToEast = (int)reader["EastID"];
-                        loc.LocationToSouth = (int)reader["SouthID"];
-                        loc.LocationToWest = (int)reader["WestID"];
+                        // Safely handle DBNulls for navigation
+                        loc.LocationToNorth = reader["NorthID"] != DBNull.Value ? Convert.ToInt32(reader["NorthID"]) : -1;
+                        loc.LocationToEast = reader["EastID"] != DBNull.Value ? Convert.ToInt32(reader["EastID"]) : -1;
+                        loc.LocationToSouth = reader["SouthID"] != DBNull.Value ? Convert.ToInt32(reader["SouthID"]) : -1;
+                        loc.LocationToWest = reader["WestID"] != DBNull.Value ? Convert.ToInt32(reader["WestID"]) : -1;
 
                         locations.Add(loc);
                     }
                 }
 
-                // --- PASS 2: LOAD INTERACTABLES (Doors, Chests) ---
-                string sqlObj = "SELECT * FROM Interactables";
-                using (var cmd = new SqlCommand(sqlObj, conn))
+                // --- PASS 2: LOAD INTERACTABLES ---
+                string sqlInt = "SELECT * FROM Interactables";
+                using (var cmd = new SqlCommand(sqlInt, conn))
                 using (var reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        var obj = new InteractableObject
+                        var interactable = new InteractableObject
                         {
-                            // Note: We don't store ID in the class yet, but we use it for logic below
                             Name = reader["Name"].ToString(),
                             X = Convert.ToDouble(reader["X"]),
                             Y = Convert.ToDouble(reader["Y"]),
                             Width = Convert.ToDouble(reader["Width"]),
                             Height = Convert.ToDouble(reader["Height"]),
-                            IsLocked = (bool)reader["IsLocked"],
-                            RequiredItem = reader["RequiredItem"] as string, // Handle NULLs safely
-                            LockedMessage = reader["LockedMessage"] as string,
-                            TargetLocationID = (int)reader["TargetLocationID"]
+                            IsLocked = reader["IsLocked"] != DBNull.Value && Convert.ToBoolean(reader["IsLocked"]),
+                            RequiredItem = reader["RequiredItem"] != DBNull.Value ? reader["RequiredItem"].ToString() : null,
+                            LockedMessage = reader["LockedMessage"] != DBNull.Value ? reader["LockedMessage"].ToString() : "It's locked.",
+                            TargetLocationID = reader["TargetLocationID"] != DBNull.Value ? Convert.ToInt32(reader["TargetLocationID"]) : 0
                         };
 
                         // Find the room this object belongs to
-                        int parentID = (int)reader["ParentLocationID"];
-                        var parentRoom = locations.FirstOrDefault(x => x.ID == parentID);
-                        if (parentRoom != null)
+                        if (reader["ParentLocationID"] != DBNull.Value)
                         {
-                            parentRoom.Interactables.Add(obj);
-                        }
+                            int parentID = Convert.ToInt32(reader["ParentLocationID"]);
+                            var parentRoom = locations.FirstOrDefault(x => x.ID == parentID);
 
-                        // TEMP: Store the DB ID temporarily to help load items inside it (Advanced technique: Use a Dictionary)
-                        // For this tutorial, we will rely on Order or assume Items load simply.
-                        // Ideally, InteractableObject should have an ID property.
+                            if (parentRoom != null)
+                            {
+                                // FIXED: Changed 'obj' to 'interactable'
+                                parentRoom.Interactables.Add(interactable);
+                            }
+                        }
                     }
                 }
 
@@ -94,29 +94,27 @@ namespace DyingFire.Services
                         {
                             Name = reader["Name"].ToString(),
                             Description = reader["Description"].ToString(),
-                            ImagePath = reader["ImagePath"] as string,
-                            Type = (ItemType)(int)reader["Type"] // Cast INT from DB to ENUM
+                            ImagePath = reader["ImagePath"] != DBNull.Value ? reader["ImagePath"].ToString() : null,
+                            Type = (ItemType)Convert.ToInt32(reader["Type"]) // Cast INT from DB to ENUM safely
                         };
 
                         // Is it on the floor?
                         if (reader["ParentLocationID"] != DBNull.Value)
                         {
-                            int roomID = (int)reader["ParentLocationID"];
+                            int roomID = Convert.ToInt32(reader["ParentLocationID"]);
                             var room = locations.FirstOrDefault(r => r.ID == roomID);
                             if (room != null) room.RoomItems.Add(item);
                         }
-                        // Is it in a chest? (This is tricky without IDs in the Interactable class)
+                        // Is it in a chest?
                         else if (reader["ParentInteractableID"] != DBNull.Value)
                         {
                             // HARD CODED FIX FOR LEARNING: 
-                            // Since our InteractableObject class doesn't have an ID property yet,
-                            // we will just put the Key in the Chest in Room 3 manually for now.
-                            // In a full commercial refactor, you would add 'public int ID' to InteractableObject.cs.
+                            int chestID = Convert.ToInt32(reader["ParentInteractableID"]);
 
-                            int chestID = (int)reader["ParentInteractableID"];
                             // Logic: Find the chest in the Ritual Room (Room 3)
                             var ritualRoom = locations.FirstOrDefault(x => x.ID == 3);
                             var chest = ritualRoom?.Interactables.FirstOrDefault(x => x.Name == "Old Chest");
+
                             if (chest != null) chest.ItemsInside.Add(item);
                         }
                     }
